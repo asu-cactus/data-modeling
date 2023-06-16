5b3ae3520152455fab366e96498b4e228b19042e
Hong Guan <hguan6@en4217518l.fulton.ad.asu.edu>
2023-06-16 08:34:41 -0700

    configure mlp.py

--- numerical/star/mlp.py
+++ numerical/star/mlp.py
@@ -7,22 +7,19 @@ from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 from collections import deque
 import os
-import logging
 
-logging.basicConfig(level=logging.INFO)
+from opacus import PrivacyEngine
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+from utils import estimate_model_size
 
 
 def get_classification_model(out_units, checkpoint=None):
     model = nn.Sequential(
-        nn.Linear(1, 1000),
+        nn.Linear(1, 256),
         nn.ReLU(),
-        nn.Linear(1000, 1000),
+        nn.Linear(256, 256),
         nn.ReLU(),
-        nn.Linear(1000, 1000),
-        nn.ReLU(),
-        nn.Linear(1000, out_units),
+        nn.Linear(256, out_units),
     )
     if checkpoint is not None:
         model.load_state_dict(torch.load(f"outputs/{checkpoint}"))
@@ -31,13 +28,11 @@ def get_classification_model(out_units, checkpoint=None):
 
 def get_regression_model(checkpoint=None):
     model = nn.Sequential(
-        nn.Linear(1, 1000),
+        nn.Linear(1, 800),
         nn.ReLU(),
-        nn.Linear(1000, 1000),
+        nn.Linear(800, 512),
         nn.ReLU(),
-        nn.Linear(1000, 1000),
-        nn.ReLU(),
-        nn.Linear(1000, 1),
+        nn.Linear(512, 1),
     )
     if checkpoint is not None:
         model.load_state_dict(torch.load(f"outputs/{checkpoint}"))
@@ -52,6 +47,8 @@ def load_data(usecols, name, dtype):
         names=[name],
         dtype=dtype,
     )
+    # Hard code for now: convert data to np.int32
+    data = data.astype(np.int32)
     return data
 
 
@@ -64,7 +61,7 @@ def to_categorical(data, name):
     return len(unique_values)
 
 
-def create_training_loader(data, name, batch_size=256):
+def create_training_loader(data, name, batch_size=64):
     # Create a TensorDataset from our data
     dataset = torch.utils.data.TensorDataset(
         torch.arange(len(data), dtype=torch.float32).unsqueeze(1),
@@ -76,7 +73,15 @@ def create_training_loader(data, name, batch_size=256):
 
 
 def train_one_epoch(
-    model, optimizer, scheduler, loss_fn, metric, epoch_index, tb_writer
+    model,
+    training_loader,
+    optimizer,
+    scheduler,
+    loss_fn,
+    metric,
+    epoch_index,
+    tb_writer,
+    privacy_engine=None,
 ):
     running_loss = 0.0
     last_loss = 0.0
@@ -104,7 +109,13 @@ def train_one_epoch(
 
     avg_loss = running_loss / len(training_loader)  # loss per batch
     scheduler.step(avg_loss)
-    logging.info(f"Epoch {epoch_index} loss: {avg_loss}, accuracy: {acc}")
+    if privacy_engine is not None:
+        epsilon = privacy_engine.get_epsilon(1e-5)
+        print(
+            f"Epoch {epoch_index} loss: {avg_loss}, accuracy: {acc}, epsilon: {epsilon}"
+        )
+    else:
+        print(f"Epoch {epoch_index} loss: {avg_loss}, accuracy: {acc} ")
     tb_writer.add_scalar("Loss/train", avg_loss, epoch_index)
     tb_writer.add_scalar("lr", optimizer.param_groups[0]["lr"], epoch_index)
     tb_writer.add_scalar("acc", acc, epoch_index)
@@ -112,7 +123,16 @@ def train_one_epoch(
     return last_loss
 
 
-def train(model, optimizer, scheduler, loss_fn, metric, epochs=10000):
+def train(
+    model,
+    training_loader,
+    optimizer,
+    scheduler,
+    loss_fn,
+    metric,
+    epochs,
+    privacy_engine=None,
+):
     # Initializing in a separate cell so we can easily add more epochs to the same run
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     writer = SummaryWriter("outputs/runs/mlp_{}".format(timestamp))
@@ -125,10 +145,18 @@ def train(model, optimizer, scheduler, loss_fn, metric, epochs=10000):
         # Make sure gradient tracking is on, and do a pass over the data
         # model.train(True)
         train_one_epoch(
-            model, optimizer, scheduler, loss_fn, metric, epoch_number, writer
+            model,
+            training_loader,
+            optimizer,
+            scheduler,
+            loss_fn,
+            metric,
+            epoch_number,
+            writer,
+            privacy_engine=privacy_engine,
         )
 
-        model_path = "outputs/model_{}_{}".format(timestamp, epoch_number)
+        model_path = f"outputs/model_{timestamp}_{epoch_number}"
         checkpoints.append(model_path)
         if len(checkpoints) > maxlen:
             old_model_path = checkpoints.popleft()
@@ -136,11 +164,7 @@ def train(model, optimizer, scheduler, loss_fn, metric, epochs=10000):
         torch.save(model.state_dict(), model_path)
 
 
-if __name__ == "__main__":
-    is_regression = False
-    usecols = 2
-    name = "dst"
-    dtype = np.int32
+def main(usecols: int, name: str, dtype, is_regression: bool, epochs: int, is_dpsgd=False):
     data = load_data(usecols, name, dtype)
     if not is_regression:
         num_cate = to_categorical(data, name)
@@ -152,7 +176,53 @@ if __name__ == "__main__":
     else:
         model = get_classification_model(num_cate)
         loss_fn = torch.nn.CrossEntropyLoss()
+    estimate_model_size(model)
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, "min")
     metric = MulticlassAccuracy()
-    train(model, optimizer, scheduler, loss_fn, metric)
+    privacy_engine = None
+    if is_dpsgd:
+        privacy_engine = PrivacyEngine()
+        # model, optimizer, training_loader = privacy_engine.make_private(
+        #     module=model,
+        #     optimizer=optimizer,
+        #     data_loader=training_loader,
+        #     noise_multiplier=1.0,
+        #     max_grad_norm=1.0,
+        # )
+        
+        model, optimizer, training_loader = privacy_engine.make_private_with_epsilon(
+            module=model,
+            optimizer=optimizer,
+            data_loader=training_loader,
+            target_epsilon=0.76,
+            target_delta=1e-5,
+            epochs=epochs,
+            max_grad_norm=1.0,
+        )
+   
+    train(
+        model,
+        training_loader,
+        optimizer,
+        scheduler,
+        loss_fn,
+        metric,
+        epochs,
+        privacy_engine=privacy_engine,
+    )
+    epsilon = privacy_engine.get_epsilon(1e-5)
+    print(f"Final epsilon: {epsilon}")
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
+    usecols = 6
+    name = "rnumber"
+    dtype = np.int32
+    is_regression = True
+    is_dpsgd = True
+
+    epochs = 10
+
+    main(usecols, name, dtype, is_regression, epochs, is_dpsgd=is_dpsgd)
