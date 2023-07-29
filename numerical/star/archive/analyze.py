897ad5b53267b70634945dc44fc8028a6868751a
Hong Guan <hguan6@asu.edu>
2023-07-29 07:43:33 -0700

    add pruning

--- numerical/star/archive/analyze.py
+++ numerical/star/archive/analyze.py
@@ -3,58 +3,70 @@ from utils import names
 
 from collections import defaultdict
 import os
+
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+
 
 def parse_pred_line(line: str):
     d = {}
     for name in names:
-        segs = line.split(f'{name}:', maxsplit=1)
-        d[name] = segs[1].split(',', maxsplit=1)[0] if len(segs) > 1 else ''
+        segs = line.split(f"{name}:", maxsplit=1)
+        d[name] = segs[1].split(",", maxsplit=1)[0] if len(segs) > 1 else ""
     return d
 
+
 def extract_incorrect_outputs(checkpoint: str):
-    """ Compare star2000.txt and checkout-x.txt """
-    predictions = load_lines(f'data/{checkpoint}.txt')
-    references = load_lines(f'data/star2000.txt')
+    """Compare star2000.txt and checkout-x.txt"""
+    predictions = load_lines(f"data/{checkpoint}.txt")
+    references = load_lines(f"data/star2000.txt")
     assert len(predictions) == len(references)
-    
+
     mismatch = defaultdict(list)
     for ref, pred in zip(references, predictions):
-        ref = {name: ref.split(f'{name}:', maxsplit=1)[1].split(
-            ',', maxsplit=1)[0]for name in names}
+        ref = {
+            name: ref.split(f"{name}:", maxsplit=1)[1].split(",", maxsplit=1)[0]
+            for name in names
+        }
         pred = parse_pred_line(pred)
         for name in names:
             if ref[name] != pred[name]:
                 mismatch[name].append((ref[name], pred[name]))
     for name in names.keys():
-        with open(f'{name}.out', 'w') as f:
-            f.writelines([f'{ref},{pred}\n' for ref, pred in mismatch[name]])
-    
+        with open(f"{name}.out", "w") as f:
+            f.writelines([f"{ref},{pred}\n" for ref, pred in mismatch[name]])
+
+
 def _extract_correct_output_rows(checkpoint: str):
-    """ Compare star2000.txt and checkout-x.txt """
-    predictions = load_lines(f'data/{checkpoint}.txt')
-    references = load_lines(f'data/star2000.txt')
+    """Compare star2000.txt and checkout-x.txt"""
+    predictions = load_lines(f"data/{checkpoint}.txt")
+    references = load_lines(f"data/star2000.txt")
     assert len(predictions) == len(references)
-    
+
     results = defaultdict(list)
     for i, (ref, pred) in enumerate(zip(references, predictions)):
-        ref = {name: ref.split(f'{name}:', maxsplit=1)[1].split(
-            ',', maxsplit=1)[0]for name in names}
+        ref = {
+            name: ref.split(f"{name}:", maxsplit=1)[1].split(",", maxsplit=1)[0]
+            for name in names
+        }
         pred = parse_pred_line(pred)
         for name in names.keys():
             if ref[name] == pred[name]:
                 results[name].append(i)
     return results
-                
+
+
 def get_overlap(cpt1: str, cpt2: str):
     d1 = _extract_correct_output_rows(cpt1)
     d2 = _extract_correct_output_rows(cpt2)
 
     for name in names.keys():
-        overlap =  set(d1[name]).intersection(d2[name])
-        print(f"""{name}:
+        overlap = set(d1[name]).intersection(d2[name])
+        print(
+            f"""{name}:
               checkpoint1 corrects: {len(d1[name])}
               checkpoint2 corrects: {len(d2[name])}
-              overlap: {len(overlap)}""")
-        
-get_overlap('checkpoint-46706', 'checkpoint-50952')
+              overlap: {len(overlap)}"""
+        )
+
+
+get_overlap("checkpoint-46706", "checkpoint-50952")
---
+++ numerical/star/utils_v2.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import multiprocessing as mp
+from random import sample
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+USECOLS = [2, 3, 4, 5, 6]
+names = {
+    "dst": np.int32,
+    "hist": np.int32,
+    "enumber": np.int32,
+    "etime": np.float64,
+    "rnumber": np.int32,
+}
+
+
+def _row_to_dict(args):
+    idx, row = args
+    patterns = [
+        "{:06d}",
+        "{:06d}",
+        "{:05d}",
+        "{:08d}",
+        "{:07d}",
+    ]
+    features = [pattern.format(value) for value, pattern in zip(row, patterns)]
+    output = ",".join(features)
+    return {"instruction": f"{idx:07d}$", "output": output}
+
+
+def load_data():
+    data = pd.read_csv(
+        "data/star2000.csv.gz",
+        header=None,
+        usecols=USECOLS,
+        names=list(names),
+        dtype=names,
+    )
+    data = data.to_numpy().astype(np.int32)
+
+    with mp.Pool(mp.cpu_count() - 2) as pool:
+        data = pool.imap(_row_to_dict, [(i, row) for i, row in enumerate(data)], 100000)
+        data = [d for d in data]
+    # with open("data/star2000.txt", "w") as f:
+    #     for idx, row in data.iterrows():
+    #         string = self._row_to_string(idx, row)
+    #         f.write(f"{string}\n")
+    #         lines.append(string)
+
+    with open("data/star2000_v2.txt", "w") as f:
+        for sample in data:
+            f.write(f"{sample['instruction']}{sample['output']}\n")
+    return data
+
+
+def sample_data(data, size):
+    return sample(data, size)
+
+
+def estimate_model_size(model):
+    """Estimate Pytorch model size in MB"""
+    size_in_mb = model.get_memory_footprint() / 1024**2
+    logger.info(f"Model size is {size_in_mb:.2f}MB")
+    return size_in_mb
+
+
+if __name__ == "__main__":
+    data = load_data()
+    print(sample_data(data, 5))
